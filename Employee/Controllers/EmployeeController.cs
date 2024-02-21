185c14ccb0d2f2af32dafe53ad1dd3355bbd6747
janthony25 <junanthony.business@gmail.com>
2024-02-21 18:47:33 +1300

    Add EmployeeController

---
+++ Employee/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Employee.Data;
+using Employee.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EmployeeController(ApplicationDbContext db)
+        {
+            _db = db;   
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmployeeList> obj = _db.employees;
+            return View(obj);
+        }
+    }
+}
