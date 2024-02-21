01f4df1b0a4fcd0f2ecc7fa7883b6c0f8a57a975
janthony25 <junanthony.business@gmail.com>
2024-02-21 18:38:08 +1300

    Add project files.

---
+++ Employee/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employee.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int employeeId { get; set; }
+        [Required]
+        public string employeeName { get; set; }
+        public int MyProperty { get; set; }
+        public string position { get; set; }    
+    }
+}
